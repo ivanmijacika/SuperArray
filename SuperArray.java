deaf62984731020969b119653b734c495aa3cea1
ivanmijacika <imijacika20@stuy.edu>
2020-11-02 19:07:36 -0500

    SuperArray created with constructor and instance variables

---
+++ SuperArray.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class SuperArray{
+
+    private String[] data;
+    private int size;
+
+    public SuperArray(){
+	data = new String[10];
+	size = 0;
+    }
+
+
+
+}
