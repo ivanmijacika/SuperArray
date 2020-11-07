851fca66bdecfb7b93316defc3993d29623981f4
ivanmijacika <imijacika20@stuy.edu>
2020-11-07 15:54:01 -0500

    equals() added to SuperArray.java and tested

--- SuperArray.java
+++ SuperArray.java
@@ -108,8 +108,12 @@ public class SuperArray{
 	return -1;
     }
 
-//    public boolean equals(SuperArray other){
-	
-  //  }
+    public boolean equals(SuperArray other){
+	if (size!=other.size) return false;
+	for (int i=0; i<size; i++){
+	    if (!data[i].equals(other.data[i])) return false;
+	}
+	return true;
+    }
 
 }
